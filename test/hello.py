807ac642da0763b30157fde543adca91c3e3fb74
zhuhaonav <781984548@qq.com>
2017-11-16 16:51:55 +0800

    1

---
+++ test/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+x="a"
+y="b"
+
+#输出
+print(x)
+print(y)
+
+from sys import path
+print('============mode import============');
+print('命令行参数为')
+# for i in sys.argv:
+#     print(i)
+
+print('\n python 路径为',path)
+
+# Python的数据类型（列表，元组，集合，字典）
