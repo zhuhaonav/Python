db1da37f42ccd1d3fe412b510141d0c0693773c5
zhuhaonav <781984548@qq.com>
2017-12-04 18:04:46 +0800

    2017/12/4

--- test/datastructure.py
+++ test/datastructure.py
@@ -1,5 +1,5 @@
 #Python数据结构
-##列表推导式
+##列表推导式 ...for...in...if
 #可以对序列里每一个元素逐个调用某方法
 vec = [2,4,6]
 vec2 = [3*x for x in vec]
@@ -21,5 +21,33 @@ print('matrix2',matrix2,'matrix3',matrix3)
 
 #del语句
 a = [-1, 1, 66.25, 333, 333, 1234.5]
+b = a.copy()
 del a[:]
-print(a)
+print(b,a)
+
+#元组，序列，集合
+a = set('abracgh')
+b = set('cenmbhg')
+print(a,a^b)#集合运算
+
+#字典创建
+dict1 = dict([('space',4139),('guido',4127),('jack',4098)])
+dict2 = dict(sape=4139, guido=4127, jack=4098)
+print(dict,dict2)
+#遍历
+for k,v in dict1.items():
+  print(v)
+#索引遍历(序列，enumerate函数)
+for i,v in enumerate(['tic','tac','toe']):
+  print(i,v)
+#同时遍历两个或多个序列
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q,a in zip(questions,answers):
+  print('What is your {0}? It is {1}.'.format(q,a,))
+#反向遍历
+for i in reversed(range(1, 10, 2)):
+  print(i,end=',')
+for i in sorted(range(1, 10, 2)):
+  print(i,end=',')
+
