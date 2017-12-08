74e170ce2a2f217ea3e3cddf0fa7c1e7d056b875
zhuhaonav <781984548@qq.com>
2017-12-08 18:06:55 +0800

    2017/12/8

--- test/object.py
+++ test/object.py
@@ -46,8 +46,68 @@ class student(people):
     #覆写父类的方法
     def speak(self):
         print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
- 
- 
- 
 s = student('ken',10,60,3)
 s.speak()
+
+class speaker():
+    topic = ''
+    name = ''
+    def __init__(self,n,t):
+        self.name = n
+        self.topic = t
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s"%(self.name,self.topic))
+ 
+#多重继承
+class sample(speaker,student):
+    a =''
+    def __init__(self,n,a,w,g,t):
+        student.__init__(self,n,a,w,g)
+        speaker.__init__(self,n,t)
+ 
+test = sample("Tim",25,80,4,"Python")
+test.speak()#方法名同，默认调用的是在括号中排前地父类的方法
+
+#父类方法重写
+class Parent:
+  def myMethod(self):
+    print('调用父类方法')
+
+class Child:
+  def myMethod(self):
+    print('调用子类方法')
+
+c=Child()
+c.myMethod()
+
+#两个下划线开头，声明该方法为私有方法，只能在类的内部调用 ，不能在类地外部调用。
+class JustCounter:
+    __secretCount = 0  # 私有变量
+    publicCount = 0    # 公开变量
+ 
+    def count(self):
+        self.__secretCount += 1
+        self.publicCount += 1
+        print (self.__secretCount)
+ 
+counter = JustCounter()
+counter.count()
+counter.count()
+print (counter.publicCount)
+#print (counter.__secretCount)  # 报错，实例不能访问私有变量
+
+#运算符重载
+class Vector:
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+ 
+   def __str__(self):
+      return 'Vector (%d, %d)' % (self.a, self.b)
+   
+   def __add__(self,other):
+      return Vector(self.a + other.a, self.b + other.b)
+ 
+v1 = Vector(2,10)
+v2 = Vector(5,-2)
+print (v1 + v2)
---
+++ test/regular.py
@@ -0,0 +1,42 @@
+#re.match只匹配字符串的开始，如果字符串开始不符合正则表达式，则匹配失败，
+# 函数返回None；而re.search匹配整个字符串，直到找到一个匹配。
+import re
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print ("matchObj.group() : ", matchObj.group())
+   print ("matchObj.group(1) : ", matchObj.group(1))
+   print ("matchObj.group(2) : ", matchObj.group(2))
+else:
+   print ("No match!!")
+
+
+searchObj = re.search( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if searchObj:
+   print ("searchObj.group() : ", searchObj.group())
+   print ("searchObj.group(1) : ", searchObj.group(1))
+   print ("searchObj.group(2) : ", searchObj.group(2))
+else:
+   print ("Nothing found!!")
+
+#检索和替换
+phone = "2004-959-559 # 这是一个电话号码"
+
+# 删除注释
+num = re.sub(r'#.*$', "", phone)
+print ("电话号码 : ", num)
+
+# 移除非数字的内容
+num = re.sub(r'\D', "", phone)
+print ("电话号码 : ", num)
+
+def double(matched):
+    value = int(matched.group('value'))
+    return str(value * 2)
+
+s = 'A23G4HFD567'
+print(re.sub('(?P<value>\d+)', double, s))#第二个参数可以是函数
